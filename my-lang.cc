5dae4cb4d1be7ecc31d03cff26dfc276a4908cf4
nebler <benklernoah@gmail.com>
2024-05-21 22:29:11 +0200

    it compiles again :D

--- my-lang.cc
+++ my-lang.cc
@@ -1,11 +1,24 @@
+#include "llvm/ADT/APFloat.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Type.h"
+#include "llvm/IR/Verifier.h"
+#include <algorithm>
 #include <cctype>
 #include <cstdio>
 #include <cstdlib>
 #include <map>
 #include <memory>
 #include <string>
-#include <utility>
 #include <vector>
+
+using namespace llvm;
 
 //===----------------------------------------------------------------------===//
 // Lexer
@@ -89,86 +102,86 @@ static int gettok()
 // Abstract Syntax Tree (aka Parse Tree)
 //===----------------------------------------------------------------------===//
 
-namespace
+/// ExprAST - Base class for all expression nodes.
+class ExprAST
 {
+public:
+    virtual ~ExprAST() = default;
+    // C++ syntax =0 means no default implementation and subclasses have to implement it.
+    // Value represents a SSA
+    virtual llvm::Value *codegen() = 0;
+};
 
-    /// ExprAST - Base class for all expression nodes.
-    class ExprAST
-    {
-    public:
-        virtual ~ExprAST() = default;
-    };
+/// NumberExprAST - Expression class for numeric literals like "1.0".
+class NumberExprAST : public ExprAST
+{
+    double Val;
 
-    /// NumberExprAST - Expression class for numeric literals like "1.0".
-    class NumberExprAST : public ExprAST
-    {
-        double Val;
+public:
+    NumberExprAST(double Val) : Val(Val) {}
+    // TODO: use a visitor pattern here
+    virtual Value *codegen();
+};
 
-    public:
-        NumberExprAST(double Val) : Val(Val) {}
-    };
+/// VariableExprAST - Expression class for referencing a variable, like "a".
+class VariableExprAST : public ExprAST
+{
+    std::string Name;
 
-    /// VariableExprAST - Expression class for referencing a variable, like "a".
-    class VariableExprAST : public ExprAST
-    {
-        std::string Name;
+public:
+    VariableExprAST(const std::string &Name) : Name(Name) {}
+};
 
-    public:
-        VariableExprAST(const std::string &Name) : Name(Name) {}
-    };
+/// BinaryExprAST - Expression class for a binary operator.
+class BinaryExprAST : public ExprAST
+{
+    char Op;
+    std::unique_ptr<ExprAST> LHS, RHS;
 
-    /// BinaryExprAST - Expression class for a binary operator.
-    class BinaryExprAST : public ExprAST
-    {
-        char Op;
-        std::unique_ptr<ExprAST> LHS, RHS;
+public:
+    BinaryExprAST(char Op, std::unique_ptr<ExprAST> LHS,
+                  std::unique_ptr<ExprAST> RHS)
+        : Op(Op), LHS(std::move(LHS)), RHS(std::move(RHS)) {}
+};
 
-    public:
-        BinaryExprAST(char Op, std::unique_ptr<ExprAST> LHS,
-                      std::unique_ptr<ExprAST> RHS)
-            : Op(Op), LHS(std::move(LHS)), RHS(std::move(RHS)) {}
-    };
+/// CallExprAST - Expression class for function calls.
+class CallExprAST : public ExprAST
+{
+    std::string Callee;
+    std::vector<std::unique_ptr<ExprAST>> Args;
 
-    /// CallExprAST - Expression class for function calls.
-    class CallExprAST : public ExprAST
-    {
-        std::string Callee;
-        std::vector<std::unique_ptr<ExprAST>> Args;
+public:
+    CallExprAST(const std::string &Callee,
+                std::vector<std::unique_ptr<ExprAST>> Args)
+        : Callee(Callee), Args(std::move(Args)) {}
+};
 
-    public:
-        CallExprAST(const std::string &Callee,
-                    std::vector<std::unique_ptr<ExprAST>> Args)
-            : Callee(Callee), Args(std::move(Args)) {}
-    };
+/// PrototypeAST - This class represents the "prototype" for a function,
+/// which captures its name, and its argument names (thus implicitly the number
+/// of arguments the function takes).
+class PrototypeAST
+{
+    std::string Name;
+    std::vector<std::string> Args;
 
-    /// PrototypeAST - This class represents the "prototype" for a function,
-    /// which captures its name, and its argument names (thus implicitly the number
-    /// of arguments the function takes).
-    class PrototypeAST
-    {
-        std::string Name;
-        std::vector<std::string> Args;
+public:
+    PrototypeAST(const std::string &Name, std::vector<std::string> Args)
+        : Name(Name), Args(std::move(Args)) {}
 
-    public:
-        PrototypeAST(const std::string &Name, std::vector<std::string> Args)
-            : Name(Name), Args(std::move(Args)) {}
+    const std::string &getName() const { return Name; }
+};
 
-        const std::string &getName() const { return Name; }
-    };
+/// FunctionAST - This class represents a function definition itself.
+class FunctionAST
+{
+    std::unique_ptr<PrototypeAST> Proto;
+    std::unique_ptr<ExprAST> Body;
 
-    /// FunctionAST - This class represents a function definition itself.
-    class FunctionAST
-    {
-        std::unique_ptr<PrototypeAST> Proto;
-        std::unique_ptr<ExprAST> Body;
-
-    public:
-        FunctionAST(std::unique_ptr<PrototypeAST> Proto,
-                    std::unique_ptr<ExprAST> Body)
-            : Proto(std::move(Proto)), Body(std::move(Body)) {}
-    };
-
-} // end anonymous namespace
+public:
+    FunctionAST(std::unique_ptr<PrototypeAST> Proto,
+                std::unique_ptr<ExprAST> Body)
+        : Proto(std::move(Proto)), Body(std::move(Body)) {}
+};
 
 //===----------------------------------------------------------------------===//
 // Parser
@@ -470,6 +483,11 @@ static void MainLoop()
         }
     }
 }
+
+static std::unique_ptr<LLVMContext> TheContext;
+static std::unique_ptr<Module> TheModule;
+static std::unique_ptr<IRBuilder<>> Builder;
+static std::map<std::string, Value *> NamedValues;
 
 int main()
 {
